70af5b37d7f99251c5dc555d16c5200d79a928af
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-17 23:59:45 +0900

    메시지, 국제화 / 스프링 메시지 소스 사용

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 
 import java.util.Locale;
 
@@ -16,18 +17,22 @@ class MessageSourceTest {
     MessageSource ms;
 
     @Test
-    void helloMessage() throws NoSuchMethodException {
+    void helloMessage() {
+        // locale: 한국인지 영어인지 지정
+        // locale 정보가 없으면 basename 에서 설정한 기본 이름 메시지 파일을 조회한다. basename 으로
+        // messages 를 지정 했으므로 messages.properties 파일에서 데이터 조회한다.
         String result = ms.getMessage("hello", null, null);
         assertThat(result).isEqualTo("안녕");
     }
 
-    // 메시지가 없는 경우, 기본 메시지
+    // 메시지가 없는 경우
     @Test
     void notFoundMessageCode() {
         assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
-                .isInstanceOf(NoSuchMethodException.class);
+                .isInstanceOf(NoSuchMessageException.class);
     }
 
+    // 기본 메시지
     @Test
     void notFoundMessageCodeDefaultMessage() {
         String result = ms.getMessage("no_code", null, "기본 메시지", null);
@@ -41,14 +46,14 @@ class MessageSourceTest {
         assertThat(result).isEqualTo("안녕 Spring");
     }
 
-    // 국제화 파일 선택1
+    // 국제화 파일 선택1 (한국어)
     @Test
     void defaultLang() {
         assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
         assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
     }
 
-    // 국제화 파일 선택2
+    // 국제화 파일 선택2 (영어)
     @Test
     void enLang() {
         assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
